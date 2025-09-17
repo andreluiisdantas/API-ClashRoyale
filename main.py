52cb6599684e88c43bd9ee73510d2df1a56efbe5
andreluiisdantas <andreluissousaa10@gmail.com>
2025-09-17 09:51:27 -0300

    Método Patch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from typing import List
-from models import Carta
+from models import Carta, CartaAtualizar
 
 app = FastAPI()
 
@@ -13,7 +13,7 @@ def mostrar_cartas():
 
 # Endpoint para adicionar carta
 @app.post("/cartas", response_model=Carta)
-def criar_carta(carta: Carta):
+async def criar_carta(carta: Carta):
 
     for item in db:
         if item.nome == carta.nome:
@@ -24,7 +24,7 @@ def criar_carta(carta: Carta):
 
 # Endpoint para ver uma carta especifica
 @app.get("/cartas/{carta_id}", response_model=Carta)
-def mostrar_especifica(carta_id: int):
+async def mostrar_especifica(carta_id: int):
     for item in db:
         if item.id == carta_id:
             return item
@@ -33,7 +33,7 @@ def mostrar_especifica(carta_id: int):
 
 # Endpoint para editar uma carta especifica
 @app.put("/cartas/{carta_id}", response_model=Carta)
-def editar_especifca(carta_id: int, carta_atualizada: Carta):
+async def editar_especifca(carta_id: int, carta_atualizada: Carta):
     
     for index, item in enumerate(db):
         if item.id == carta_id:
@@ -42,9 +42,27 @@ def editar_especifca(carta_id: int, carta_atualizada: Carta):
             return carta_atualizada
     raise HTTPException(status_code=404, detail="Item não existe")
 
+# Endpoint para editar uma carta especifica
+@app.patch("/cartas/{carta_id}", response_model=Carta)
+async def editar_campo(carta_id: int, atualizacoes: CartaAtualizar):
+    carta_existente = db.get(carta_id)
+    if not carta_existente:
+        raise HTTPException(status_code=404, detail="Carta não encontrada")
+    
+    dados_novos = atualizacoes.dict(exclude_unset=True)
+
+    if not dados_novos:
+        return carta_existente
+    
+    carta_atualizada = carta_existente.copy(update=dados_novos)
+    
+    db[carta_id] = carta_atualizada
+    return carta_atualizada
+    
+
 # Endpoint para deletar carta especifica
 @app.delete("/cartas/{carta_id}", response_model=Carta)
-def deletar_carta(carta_id: int):
+async def deletar_carta(carta_id: int):
     
     for index, item in enumerate(db):
         if item.id == carta_id:
--- models.py
+++ models.py
@@ -11,3 +11,13 @@ class Carta(BaseModel):
     dano: int
     nivel: int
     description: Optional[str] = None
+
+class CartaAtualizar(BaseModel):
+    nome: Optional[str] = None
+    tipo: Optional[str] = None
+    raridade: Optional[str] = None
+    custo: Optional[int] = None
+    vida: Optional[int] = None
+    dano: Optional[int] = None
+    nivel: Optional[int] = None
+    description: Optional[str] = None
